2e80fd20048834d32ceaf04fd0e3998d06c50013
asaco19 <127094395+asaco19@users.noreply.github.com>
2023-04-23 10:42:06 +0900

    条件分岐編

--- JS/script.js
+++ JS/script.js
@@ -1,25 +1,30 @@
 //let lists = document.getElementsByClassName("pull-down-list")
 //console.log(lists)
 
-//const pullDownButton = document.getElementById("lists")
-//console.log(pullDownButton);
+const lists = document.getElementById('lists')
+const pulldown = document.getElementById("pull-down")
 
-var lists = document.getElementById('lists')
 
 lists.addEventListener('mouseover',function(){
     console.log('クリック前:青');
-    lists.setAttribute("style","background-color:blue;");
-
+    this.setAttribute("style","background-color:blue;");
 })
 
 lists.addEventListener('click',function(){
     console.log('クリック:緑');
     lists.setAttribute("style","background-color:green;");
-
+    
+    if (pulldown.getAttribute("style") == "display:block;"){
+        pulldown.removeAttribute("style","display:block;")
+        console.log("非表示")
+} else {
+    pulldown.setAttribute("style","display:block;")
+    console.log("表示")
+}
 })
 
 lists.addEventListener('mouseout',function(){
     console.log('完了');
-    lists.removeAttribute("style","background-color:red;");
+    this.removeAttribute("style","background-color:red;");
 })
 
