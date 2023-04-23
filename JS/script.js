2564352fc03a03820851210a71afc75e5dfd0ba7
asaco19 <127094395+asaco19@users.noreply.github.com>
2023-04-23 09:33:11 +0900

    イベント発火　基礎編

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,25 @@
 //let lists = document.getElementsByClassName("pull-down-list")
 //console.log(lists)
 
-const pullDownButton = document.getElementById("lists")
-console.log(pullDownButton);
+//const pullDownButton = document.getElementById("lists")
+//console.log(pullDownButton);
 
 var lists = document.getElementById('lists')
 
 lists.addEventListener('mouseover',function(){
-    console.log('クリック前');
+    console.log('クリック前:青');
+    lists.setAttribute("style","background-color:blue;");
+
 })
 
 lists.addEventListener('click',function(){
-    console.log('クリック');
+    console.log('クリック:緑');
+    lists.setAttribute("style","background-color:green;");
+
 })
 
 lists.addEventListener('mouseout',function(){
     console.log('完了');
+    lists.removeAttribute("style","background-color:red;");
 })
+
